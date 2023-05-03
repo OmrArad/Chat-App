4dc04b1f811ed5a13951b3d855acc15b0230e3ab
OmrArad <omerarad2@gmail.com>
2023-05-03 14:27:44 +0300

    Upper Section component

--- chatapp/src/ChatPage/LeftColumn/LeftColumn.js
+++ chatapp/src/ChatPage/LeftColumn/LeftColumn.js
@@ -1,6 +1,6 @@
 import ContactItem from '../ContactItem/ContactItem';
 import contacts from '../ContactItem/contacts';
-import my_pic from '../ContactItem/profile_pics/my_pic.png';
+import UpperSection from './UpperSection/UpperSection';
 
 function LeftColumn() {
 
@@ -11,35 +11,7 @@ function LeftColumn() {
     return (
         <div className="col-md-3">
             <div className="card" id="chat-card">
-                <div className="d-flex flex-column align-items-center mb-3">
-                    <h1 className="mb-3">Chats</h1>
-                    <img src={my_pic} className="rounded-circle mb-3" alt="Your Image" width="100"
-                        height="100"></img>
-                    <button type="button" className="btn btn-primary" data-toggle="modal"
-                        data-target="#addContactModal">+</button>
-                    <div className="modal fade" id="addContactModal" tabIndex="-1" role="dialog"
-                        aria-labelledby="addContactModalLabel" aria-hidden="true">
-                        <div className="modal-dialog" role="document">
-                            <div className="modal-content">
-                                <div className="modal-header">
-                                    <h5 className="modal-title" id="addContactModalLabel">Add new contact</h5>
-                                    <button type="button" className="btn-close" data-dismiss="modal"
-                                        aria-label="Close">
-                                    </button>
-                                </div>
-                                <div className="modal-body">
-                                    <form>
-                                        <div className="mb-3 input-group">
-                                            <input type="text" className="form-control" placeholder="Enter name"
-                                                name="name"></input>
-                                            <button type="submit" className="btn btn-primary">Add</button>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <UpperSection />
 
                 <form className="mb-3">
                     <div className="input-group">
---
+++ chatapp/src/ChatPage/LeftColumn/UpperSection/UpperSection.js
@@ -0,0 +1,38 @@
+import my_pic from '../../ContactItem/profile_pics/my_pic.png';
+
+function UpperSection() {
+
+    return (
+        <div className="d-flex flex-column align-items-center mb-3">
+            <h1 className="mb-3">Chats</h1>
+            <img src={my_pic} className="rounded-circle mb-3" alt="Your Image" width="100"
+                height="100"></img>
+            <button type="button" className="btn btn-primary" data-toggle="modal"
+                data-target="#addContactModal">+</button>
+            <div className="modal fade" id="addContactModal" tabIndex="-1" role="dialog"
+                aria-labelledby="addContactModalLabel" aria-hidden="true">
+                <div className="modal-dialog" role="document">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="addContactModalLabel">Add new contact</h5>
+                            <button type="button" className="btn-close" data-dismiss="modal"
+                                aria-label="Close">
+                            </button>
+                        </div>
+                        <div className="modal-body">
+                            <form>
+                                <div className="mb-3 input-group">
+                                    <input type="text" className="form-control" placeholder="Enter name"
+                                        name="name"></input>
+                                    <button type="submit" className="btn btn-primary">Add</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default UpperSection;
