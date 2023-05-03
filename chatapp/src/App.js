38eb0b18d9a2a856986f64c5d908917caaa2dea3
OmrArad <omerarad2@gmail.com>
2023-05-03 11:12:32 +0300

    React App

---
+++ chatapp/src/App.js
@@ -0,0 +1,19 @@
+// import './App.css';
+import InputFieldItem from './InputFieldItem/InputFieldItem';
+import inputs from './InputFieldItem/inputs';
+// import ChatPage from './ChatPage/ChatPage';
+import LoginPage from './LoginPage/LoginPage';
+
+function App() {
+
+  const inputList = inputs.map((input, key) => {
+    return <InputFieldItem {...input} key={key} />
+  })
+
+  return (
+    // <ChatPage />
+    <LoginPage />
+  );
+}
+
+export default App;
