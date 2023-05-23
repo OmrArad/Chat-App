9b3cfd8cd9ce9ba79716914148c9e230bce5d8c3
OmrArad <omerarad2@gmail.com>
2023-05-23 12:08:27 +0300

    displayName

--- chatapp/src/RegistrationPage/RegistrationPage.js
+++ chatapp/src/RegistrationPage/RegistrationPage.js
@@ -92,7 +92,7 @@ function RegistrationPage() {
                             handleBlur={handleVerifyChange} error={errors.verify} />
             {/* Input field for display name */}
             <InputFieldItem title={"Display name"} type={"text"} id={"displayname-input"} placeholder={"Enter display name"}
-                            handleBlur={handleDisplayNameChange} error={errors.displayname}/>
+                            handleBlur={handleDisplayNameChange} error={errors.displayName}/>
             {/* Input field for uploading profile picture */}
             <InputFieldItem title={"Picture"} type={"file"} id={"upload-picture-input"} placeholder={""}
                             handleChange={handlePictureUpload} error={errors.picture} />
