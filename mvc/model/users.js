6658dfa29b6f0d1f62eb85d67578a880ff08c2fa
akivazev <akivazev@protonmail.com>
2023-05-29 15:27:43 +0300

    created basic user model

---
+++ mvc/model/users.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const User = new Schema({
+    username: {
+        type: String,
+        nullable: true
+    },
+    password: {
+        type: String,
+        nullable: true
+    },
+    displayName: {
+        type: String,
+        nullable: true
+    },
+    profilePic: {
+        type: String,
+        nullable: true
+    }
+});
+
+module.exports = mongoose.model('User', User)
