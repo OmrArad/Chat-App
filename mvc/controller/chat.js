95ba0b07574b3b4965c2a89ae21916ef18d4839b
Erez Gilat <erezgilat@gmail.com>
2023-05-31 19:48:51 +0300

    try and catch

--- mvc/controller/chat.js
+++ mvc/controller/chat.js
@@ -1,10 +1,11 @@
-import { getUserChats } from "../services/chat";
+import { getUserChats } from "../services/chat.js";
 
 function chat(req, res) {
   if (req.headers.authorization) {
     // Retrieve user chats based on the authorization header
+    // fetch the user name from the authorization header
+    let username = req.headers.authorization.split(" ")[1]; //????
     let chats = getUserChats(req.headers.authorization);
-
     if (chats != null) {
       if (req.method === "GET") {
         // Return all chats
--- mvc/controller/login.js
+++ mvc/controller/login.js
@@ -1,12 +1,12 @@
 import findByUsername from '../services/userNamePass.js';
 
 function login(req, res) {
+  try {
   const user = findByUsername(req.body.username, req.body.password); // Assuming findByUsername is a function that retrieves user data
-  if (user != null) {
     res.setHeader('Content-Type', 'text/plain; charset=utf-8');
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.status(200).send(user);
-  } else {
+  } catch (error) {
     res.status(404).send('Invalid username and/or password');
   }
 }
