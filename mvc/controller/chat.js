8918b1fa58b556ef9ba276ec484b9fc4f637a0d4
Erez Gilat <erezgilat@gmail.com>
2023-06-02 19:01:48 +0300

    outline of chats

--- mvc/controller/chat.js
+++ mvc/controller/chat.js
@@ -1,70 +1,26 @@
 import jwt from 'jsonwebtoken';
 import Chat from '../model/chat.js';
 
-export function chat(req, res) {
-  if (req.headers.authorization) {
-    let token = req.headers.authorization.split(" ")[1];
-    // let decodedToken = decodeToken(token);
-    let decodedToken = jwt.decode(token);
-    let chats = getUserChats(decodedToken.username);
-    if (chats != null) {
-      if (req.method === "GET") {
-        // Return all chats
-        res.status(200).json(chats);
-      } else if (req.method === "POST") {
-        // Create a new chat
-        const newChat = req.body;
-        const chat = new Chat(newChat);
-        chat.save()
-          .then(savedChat => {
-            chats.push(savedChat);
-            res.status(200).json(savedChat);
-          })
-          .catch(error => {
-            res.status(500).json({ error: 'Failed to create chat' });
-          });
-      } else {
-        res.status(405).send("Method Not Allowed");
-      }
-    } else {
-      res.status(404).send("Invalid username and/or password");
-    }
-  } else {
-    res.redirect("/login");
-  }
+
+export const retriveListOfChats = async (req, res) => {
+
 }
 
-export function getMessagesById(req, res) {
-  const chatId = req.params.chatId;
-  Chat.findById(chatId)
-    .then(chat => {
-      if (chat) {
-        res.status(200).json(chat.messages);
-      } else {
-        res.status(404).json({ error: 'Chat not found' });
-      }
-    })
-    .catch(error => {
-      res.status(500).json({ error: 'Failed to retrieve messages' });
-    });
+export const createNewChat = async (req, res) => {
 }
 
-export function newMessageById(req, res) {
-  const chatId = req.params.chatId;
-  const message = req.body;
-  Chat.findByIdAndUpdate(
-    chatId,
-    { $push: { messages: message } },
-    { new: true }
-  )
-    .then(updatedChat => {
-      if (updatedChat) {
-        res.status(200).json(updatedChat);
-      } else {
-        res.status(404).json({ error: 'Chat not found' });
-      }
-    })
-    .catch(error => {
-      res.status(500).json({ error: 'Failed to add new message' });
-    });
+export const retriveChatById = async (req, res) => {
+  
 }
+
+export const deleteChatById = async (req, res) => {
+}
+
+export const createNewMessageInChat = async (req, res) => {
+}
+
+export const retriveAllMessagesInChat = async (req, res) => {
+
+}
+
+export default { retriveListOfChats, }
--- mvc/routes/chat.js
+++ mvc/routes/chat.js
@@ -1,12 +1,16 @@
 import express from 'express';
-import {chat, getMessagesById, newMessageById} from '../controller/chat.js';
+import { retriveListOfChats, createNewChat, retriveChatById, deleteChatById, createNewMessageInChat, retriveAllMessagesInChat } from '../controller/chat.js';
 import { isLoggedIn } from '../controller/login.js';
 
 
 const router = express.Router();
 
-router.get('/', isLoggedIn, chat)
-router.get('/:id/Messages', isLoggedIn, getMessagesById)
-router.post('/:id/Messages', isLoggedIn, newMessageById)
+router.get('/', isLoggedIn, retriveListOfChats)
+router.post('/', isLoggedIn, createNewChat)
+router.get('/:id', isLoggedIn, retriveChatById)
+router.delete('/:id', isLoggedIn, deleteChatById)
+router.get('/:id/Messages', isLoggedIn, createNewMessageInChat)
+router.post('/:id/Messages', isLoggedIn, retriveAllMessagesInChat)
+
 
 export default router;
